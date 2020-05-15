cc6c4459598dd0f080d01bc9ddfed9451b729792
Oleksandr Obukhivskyi <alexobuhovskiy@gmail.com>
2020-05-15 13:46:34 +0300

    initial commit

---
+++ ProjectManagementSystem.Api/Controllers/ProjectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagementSystem.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
