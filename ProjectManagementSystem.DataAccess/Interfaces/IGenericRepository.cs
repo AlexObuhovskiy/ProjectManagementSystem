d3409431a914b56cef877c7cab167e5b5732d9ee
Oleksandr Obukhivskyi <oobukhivskyi@eshopworld.com>
2020-05-17 12:01:19 +0300

    added GenericRepository to DataAccess

---
+++ ProjectManagementSystem.DataAccess/Interfaces/IGenericRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace ProjectManagementSystem.DataAccess.Interfaces
+{
+    /// <summary>
+    /// Interface IGenericRepository
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the T entity.</typeparam>
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// DB set for TEntity
+        /// </summary>
+        DbSet<TEntity> DbSet { get; }
+
+        /// <summary>
+        /// Gets the specified predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="include">The include.</param>
+        /// <returns>Task&lt;TEntity[]&gt;.</returns>
+        Task<TEntity[]> Get(
+            Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null);
+
+        /// <summary>
+        /// Gets entity by id asynchronous.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<TEntity> GetByIdAsync(object id);
+
+        /// <summary>
+        /// Inserts new entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task InsertAsync(TEntity entity);
+
+        /// <summary>
+        /// Deletes entity by id
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(object id);
+
+        /// <summary>
+        /// Deletes entity
+        /// </summary>
+        /// <param name="entityToDelete"></param>
+        void Delete(TEntity entityToDelete);
+
+        /// <summary>
+        /// Updates existing entity
+        /// </summary>
+        /// <param name="entityToUpdate"></param>
+        void Update(TEntity entityToUpdate);
+    }
+}
---
+++ ProjectManagementSystem.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using ProjectManagementSystem.DataAccess.Interfaces;
+using ProjectManagementSystem.DataAccess.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace ProjectManagementSystem.DataAccess.Repositories
+{
+    /// <summary>
+    /// Class GenericRepository.
+    /// Implements the <see cref="IGenericRepository{TEntity}" />
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the t entity.</typeparam>
+    /// <seealso cref="IGenericRepository{TEntity}" />
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        /// <inhertidoc/>
+        protected readonly ProjectManagementSystemContext Context;
+
+        /// <inhertidoc/>
+        public DbSet<TEntity> DbSet { get; }
+
+        /// <inhertidoc/>
+        public GenericRepository(ProjectManagementSystemContext context)
+        {
+            Context = context;
+            DbSet = context.Set<TEntity>();
+        }
+
+        /// <inhertidoc/>
+        public Task<TEntity[]> Get(
+            Expression<Func<TEntity, bool>> predicate = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include = null)
+        {
+            IQueryable<TEntity> query = DbSet;
+
+            if (include != null)
+            {
+                query = include(query);
+            }
+
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            return query.ToArrayAsync();
+        }
+
+        /// <inhertidoc/>
+        public Task<TEntity> GetByIdAsync(object id)
+        {
+            return DbSet.FindAsync(id).AsTask();
+        }
+
+        /// <inhertidoc/>
+        public Task InsertAsync(TEntity entity)
+        {
+            return DbSet.AddAsync(entity).AsTask();
+        }
+
+        /// <inhertidoc/>
+        public void Delete(object id)
+        {
+            TEntity entityToDelete = DbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        /// <inhertidoc/>
+        public void Delete(TEntity entityToDelete)
+        {
+            if (Context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Attach(entityToDelete);
+            }
+
+            DbSet.Remove(entityToDelete);
+        }
+
+        /// <inhertidoc/>
+        public void Update(TEntity entityToUpdate)
+        {
+            DbSet.Attach(entityToUpdate);
+            Context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
